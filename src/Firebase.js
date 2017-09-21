6e53f26e5378efff3dc3b7907b08fba09fe4d85b
felipe <felipe.leonco@gmail.com>
2017-09-21 22:48:17 +0300

    no info

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyC11dHq6z2EZAbl1dbhyyqYvaCCKn1IAJ0",
-  authDomain: "goals-268bc.firebaseapp.com",
-  databaseURL: "https://goals-268bc.firebaseio.com",
-  projectId: "goals-268bc",
-  storageBucket: "goals-268bc.appspot.com",
-  messagingSenderId: "638650988654"
+  apiKey: 
+  authDomain: 
+  databaseURL: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
 };
 
   export const firebaseApp = firebase.initializeApp(config);
